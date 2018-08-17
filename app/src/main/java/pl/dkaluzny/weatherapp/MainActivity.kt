5d2dc68dbad1a07b8c12352c0e52d7e7e82b28f4
kalug89 <kaluzny_david@hotmail.com>
2018-08-17 22:12:18 +0200

    Parse json

--- app/src/main/java/pl/dkaluzny/weatherapp/MainActivity.kt
+++ app/src/main/java/pl/dkaluzny/weatherapp/MainActivity.kt
@@ -2,14 +2,48 @@ package pl.dkaluzny.weatherapp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import okhttp3.OkHttpClient
-import okhttp3.Request
+import com.google.gson.GsonBuilder
+import okhttp3.*
+import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        fetchJson()
     }
 
+    fun fetchJson() {
+        println("Attempting to Fetch JSON")
+        val url = "https://api.letsbuildthatapp.com/youtube/home_feed"
+
+        val request = Request.Builder().url(url).build()
+
+        val client = OkHttpClient()
+        client.newCall(request).enqueue(object : Callback {
+            override fun onResponse(call: Call?, response: Response?) {
+                val body = response?.body()?.string()
+                println(body)
+
+                val gson = GsonBuilder().create()
+                val homeFeed = gson.fromJson(body, HomeFeed::class.java)
+            }
+
+            override fun onFailure(call: Call?, e: IOException?) {
+                println("Failed to execute request")
+            }
+
+        })
+
+
+    }
+
+
 }
+
+class HomeFeed(val videos: List<Video>)
+
+class Video(val id: Int, val name: String, val link: String, val imageUrl: String, val numbeOfviews: Int)
+
+class Channel(val name: String, val profileImageUrl: String, val numberOfSubscribers: Int)
